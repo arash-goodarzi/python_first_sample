dd84123055e4c6dc782b6bd38c51cd6d86c64fb2
Arash Goodarzi <arash13579@yahoo.com>
2019-10-13 23:30:09 -0400

    init commit

---
+++ first1.py
@@ -0,0 +1,21 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+print("Hi")
+
+# r = requests.get("http://www.google.com")
+
+# print(r.status_code)
+def abc():
+    i = 2 + 2
+    print("ggg")
+
+
+def bcd():
+    print("ttt")
+
